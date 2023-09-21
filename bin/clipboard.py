63bb364856da70be5b81f273b7147c53a34c0c94
Raymond Yee <raymond.yee@gmail.com>
2023-09-21 09:11:29 -0700

    commit clipboard.py before deleteing old code that used the pasteboard library

--- bin/clipboard.py
+++ bin/clipboard.py
@@ -13,6 +13,18 @@ STRING_TYPES = (
 )
 BINARY_TYPES = (pasteboard.PDF, pasteboard.PNG, pasteboard.TIFF)
 
+from rdhyee_utils.clipboard.macos import GeneralPasteboard, ptypes, PasteboardItem
+
+
+def escape_non_ascii_html(text):
+    result = []
+    for char in text:
+        if ord(char) > 127:
+            result.append(f"&#{ord(char)};")
+        else:
+            result.append(char)
+    return "".join(result)
+
 
 def pb_contents():
     pb = pasteboard.Pasteboard()
@@ -26,16 +38,16 @@ def pb_contents():
     return contents
 
 
-def pb_md_to_html(md=None):
+def ru_md_to_html(md=None):
     """
     if there is string content on the clipboard, convert it to HTML and place it on clipboard
     optionally, pass in the markdown instead of reading text from the clipboard
     """
 
-    pb = pasteboard.Pasteboard()
+    pb = GeneralPasteboard()
 
     if md is None:
-        content = pb.get_contents(type=pasteboard.String)
+        content = pb.get_string(ptypes.String)
     else:
         content = md
 
@@ -44,22 +56,58 @@ def pb_md_to_html(md=None):
             content, "html", format="markdown+task_lists+backtick_code_blocks"
         )
 
-        # extra_args=['-s']
-        result = pb.set_contents(html.encode("utf-8"), type=pasteboard.HTML)
-        return (result, content, pb.get_contents(type=pasteboard.HTML))
+        result = pb.set_content(
+            [
+                PasteboardItem(
+                    [
+                        (ptypes.HTML, escape_non_ascii_html(html)),
+                        (ptypes.String, content),
+                    ]
+                )
+            ]
+        )
+
+        # result = pb.set_string(escape_non_ascii_html(html), ptypes.HTML)
+
+        return (result, content, pb.get_string(t=ptypes.HTML))
     else:
         return (False, content, "")
+
+
+# def pb_md_to_html(md=None):
+#     """
+#     if there is string content on the clipboard, convert it to HTML and place it on clipboard
+#     optionally, pass in the markdown instead of reading text from the clipboard
+#     """
+
+#     pb = pasteboard.Pasteboard()
+
+#     if md is None:
+#         content = pb.get_contents(type=pasteboard.String)
+#     else:
+#         content = md
+
+#     if content:
+#         html = pypandoc.convert_text(
+#             content, "html", format="markdown+task_lists+backtick_code_blocks"
+#         )
+
+#         # extra_args=['-s']
+#         result = pb.set_contents(html.encode("utf-8"), type=pasteboard.HTML)
+#         return (result, content, pb.get_contents(type=pasteboard.HTML))
+#     else:
+#         return (False, content, "")
 
 
 # attribute removal techniaque from
 # [Pandoc: Convert Markdown to HTML *without* any HTML attributes - Stack Overflow](https://stackoverflow.com/questions/53403041/pandoc-convert-markdown-to-html-without-any-html-attributes/53412036#53412036)
 
 
-def pb_html_to_md(html=None):
-    pb = pasteboard.Pasteboard()
+def ru_html_to_md(html=None):
+    pb = GeneralPasteboard()
 
     if html is None:
-        content = pb.get_contents(type=pasteboard.HTML)
+        content = pb.get_string(t=ptypes.ABBR_TYPES["HTML"])
     else:
         content = html
 
@@ -68,13 +116,36 @@ def pb_html_to_md(html=None):
             content,
             "markdown+fenced_code_blocks+backtick_code_blocks+intraword_underscores-header_attributes-link_attributes-native_divs-native_spans",
             format="html",
-            extra_args=["--atx-headers", "--lua-filter=remove-attr.lua"],
+            extra_args=["--lua-filter=remove-attr.lua"],
         )
 
-        result = pb.set_contents(md, type=pasteboard.String)
-        return (result, content, pb.get_contents(type=pasteboard.String))
+        # result = pb.set_contents(md, type=pasteboard.String)
+        result = pb.set_content([PasteboardItem([(ptypes.ABBR_TYPES["String"], md)])])
+        return (result, content, pb.get_string())
     else:
         return (False, content, "")
+
+
+# def pb_html_to_md(html=None):
+#     pb = pasteboard.Pasteboard()
+
+#     if html is None:
+#         content = pb.get_contents(type=pasteboard.HTML)
+#     else:
+#         content = html
+
+#     if content:
+#         md = pypandoc.convert_text(
+#             content,
+#             "markdown+fenced_code_blocks+backtick_code_blocks+intraword_underscores-header_attributes-link_attributes-native_divs-native_spans",
+#             format="html",
+#             extra_args=["--atx-headers", "--lua-filter=remove-attr.lua"],
+#         )
+
+#         result = pb.set_contents(md, type=pasteboard.String)
+#         return (result, content, pb.get_contents(type=pasteboard.String))
+#     else:
+#         return (False, content, "")
 
 
 @click.group()
@@ -89,7 +160,8 @@ def cli(debug):
     "--content", default=None, help="convert content as markdown instead of clipboard"
 )
 def md_to_html(content):
-    pb_md_to_html(content)
+    ru_md_to_html(content)
+    # pb_md_to_html(content)
 
 
 @cli.command()
@@ -97,7 +169,7 @@ def md_to_html(content):
     "--content", default=None, help="convert content as HTML instead of clipboard"
 )
 def html_to_md(content):
-    pb_html_to_md(content)
+    ru_html_to_md(content)
 
 
 if __name__ == "__main__":
